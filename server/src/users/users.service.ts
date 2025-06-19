9d3eeb0ffb2ecf7a484fffb57ed80b1390ee3e4e
silence <makskomar2002@gmail.com>
2025-06-19 13:59:01 +0300

    add auto setting for all user

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -6,61 +6,77 @@ import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-    constructor(private readonly databaseService: DatabaseService) { }
-    async createUser(createUserDto: CreateUserDto){
-        return this.databaseService.user.create({
-            data: createUserDto
-        })
-    }
-
-    async findAll( role? : Role){
-        if (role) return this.databaseService.user.findMany({
-            where: {
-              role,
-            }
-          })
-        return this.databaseService.user.findMany()
-    }
-
-    async findByEmail(email: string){
-        if (!email) {
-            throw new Error('Email must be provided');
-        }
-        return this.databaseService.user.findUnique({
-            where: {
-                email,
-            },
-            select: {
-                id: true,
-                email: true,
-                password: true,
-                role: true,  
-              },
-        });
-    }
-
-    async findById(id: number){
-        return this.databaseService.user.findUnique({
-            where: {
-                id,
-            }
-        })
-    }
-
-    async updateUser(id: number, updateUserDto: UpdateUserDto) {
-        return this.databaseService.user.update({
-          where: {
-            id,
+  constructor(private readonly databaseService: DatabaseService) {}
+  async createUser(createUserDto: CreateUserDto) {
+    return this.databaseService.user.create({
+      data: {
+        email: createUserDto.email,
+        username: createUserDto.username,
+        password: createUserDto.password,
+        setting: {
+          create: {
+            global_language: 'UA',
+            curent_language: 'EN',
+            purposes: ['none'],
+            current_level: 'none',
           },
-          data: updateUserDto,
-        })
-      }
+        },
+      },
+      include: {
+        setting: true,
+      },
+    });
+  }
 
-    async deleteUser(id:number){
-        return this.databaseService.user.delete({
-            where:{
-                id,
-            }
-        })
+  async findAll(role?: Role) {
+    if (role)
+      return this.databaseService.user.findMany({
+        where: {
+          role,
+        },
+      });
+    return this.databaseService.user.findMany();
+  }
+
+  async findByEmail(email: string) {
+    if (!email) {
+      throw new Error('Email must be provided');
     }
+    return this.databaseService.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+        email: true,
+        password: true,
+        role: true,
+      },
+    });
+  }
+
+  async findById(id: number) {
+    return this.databaseService.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async updateUser(id: number, updateUserDto: UpdateUserDto) {
+    return this.databaseService.user.update({
+      where: {
+        id,
+      },
+      data: updateUserDto,
+    });
+  }
+
+  async deleteUser(id: number) {
+    return this.databaseService.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
